380ea95f4989748ba239b26c290aa4b3619dbd7e
levannghia <meoem2712@gmail.com>
2025-03-19 20:49:27 +0700

    Create Profile Screen

--- src/screens/dashboard/ProfileScreen.tsx
+++ src/screens/dashboard/ProfileScreen.tsx
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React, { FC } from 'react'
+import CustomSafeAreaView from '../../components/global/CustomSafeAreaView'
+import { Colors } from '../../constants/Colors'
+import CustomGradient from '../../components/global/CustomGradient'
 
 const ProfileScreen: FC = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <CustomSafeAreaView style={styles.container}>
+      <CustomGradient position='bottom'/>
+    </CustomSafeAreaView>
   )
 }
 
 export default ProfileScreen
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 0,
+    overflow: 'hidden',
+    paddingTop: 10,
+    paddingVertical: 0,
+    color: Colors.background
+  },
+  indicatorStyle: {
+    backgroundColor: 'white',
+    height: 0.8
+  },
+  noOpacity: {
+    shadowOpacity: 0,
+    elevation: 0,
+    borderWidth:0
+  },
+  tabBar: {
+    width: '33%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10
+  }
+})
